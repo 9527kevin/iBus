8a011183fb43524fa24522a566e42a5771980069
yanghua <yanghua1127@gmail.com>
2013-06-21 19:05:02 +0800

    add sinaweibo sdk

---
+++ iBus-iPhone/iBus-iPhone/SNSApiOAuthConst.h
@@ -0,0 +1,11 @@
+//
+//  SNSApiOAuthConst.h
+//  iBus-iPhone
+//
+//  Created by yanghua on 6/21/13.
+//  Copyright (c) 2013 yanghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
